f1327d3b51b419985a185c1ff5cfcd9c772e43b8
Giuse Petroso <g.petroso@gmail.com>
2020-09-26 15:12:38 +0200

    pippo

--- lib/loquacious.dart
+++ lib/loquacious.dart
@@ -6,28 +6,36 @@ class LqModel {
   /* 
     table propery for defining the sqlite table to query
    */
-  static String table;
+  String table;
 
   /* 
     constructor
    */
   LqModel() {
     ReCase rc = ReCase(this.runtimeType.toString());
-    LqModel.table = rc.snakeCase;
+    this.table = rc.snakeCase;
   }
 
-  /* static methods */
-  static List<Map<String, dynamic>> all() {
-    return LqQueryBuilder(LqModel.table).all();
+  /* 
+    all
+    retrieve all the collection
+   */
+  List<Map<String, dynamic>> all() {
+    return LqQueryBuilder(this.table).all();
   }
 }
 
 class LqQueryBuilder {
   String _query;
+
   String _table;
   LqQueryBuilder(this._table);
 
-  List<Map<String, dynamic>> all() {
+  List<LqQueryWhereClause> whereList;
+  List<LqQueryOrderClause> orderList;
 
-  }
+  List<Map<String, dynamic>> all() {}
 }
+
+class LqQueryWhereClause {}
+class LqQueryOrderClause {}
