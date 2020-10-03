49e8db5fc8d264b1114032ffb117963f503cda30
Giuse Petroso <g.petroso@gmail.com>
2020-10-03 18:04:59 +0200

    wip

--- lib/loquacious.dart
+++ lib/loquacious.dart
@@ -1,6 +1,7 @@
 library loquacious;
 
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sqflite/sqflite.dart';
 
 enum DBType {
@@ -26,8 +27,38 @@ class LqModel {
     all
     retrieve all the collection
    */
-  List<Map<String, dynamic>> all() {
+  List<Map<String, dynamic>> all() {}
+}
 
+// LOQUACIOUS DATABASE MANAGEMENT
+class LoquaciousDatabaseManager {
+  static Map<String, Database> dbMap = {};
+  static Map<String, int> versionMap = {};
+
+  static Future<Database> getDB(String databaseName) async {
+    if (!dbMap.containsKey(databaseName)) {
+      await loadVersion(databaseName); // load version
+      dbMap[databaseName] = await openDatabase(
+        "$databaseName.db",
+        version: versionMap[databaseName],
+        onConfigure: (Database db) async {
+          await db.execute('PRAGMA foreign_keys = ON');
+        },
+        onCreate: LoquaciousMigrationManager.createMigrationClosure,
+        onUpgrade: LoquaciousMigrationManager.upgradeMigrationClosure,
+      );
+    }
+    return dbMap[databaseName];
+  }
+
+  static Future<void> loadVersion(String databaseName) async {
+    if (versionMap.containsKey(databaseName)) return;
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (prefs.containsKey('loquacious_db_version:$databaseName')) {
+      versionMap[databaseName] = prefs.getInt('loquacious_db_version:$databaseName');
+    } else {
+      versionMap[databaseName] = 1;
+    }
   }
 }
 
@@ -35,13 +66,20 @@ class LqModel {
 class LoquaciousQueryBuilder {
   final databaseName;
   LoquaciousQueryBuilder(this.databaseName);
-
-  Future<void> createTable(String tableName) async {
-    var db = await openDatabase("${this.databaseName}.db");
-  }
 }
 
 // MIGRATION MANAGEMENT
+class LoquaciousMigrationManager {
+  Map<int, String> migrationsMap = {};
+  Future<List<LoquaciousMigration>> getMigrations() {}
+
+  static Future<void> createMigrationClosure(Database db, int version) async {
+
+  
+  }
+  static Future<void> upgradeMigrationClosure(Database db, int oldVersion, int newVersion) async {}
+}
+
 class LoquaciousMigration {
   final String databaseName;
   final String tableName;
@@ -50,7 +88,6 @@ class LoquaciousMigration {
     @required this.tableName,
   });
 
-  Future<void> up() async {
-    
-  }
+  Future<List<String>> up() async {}
+  Future<List<String>> down() async {}
 }
